21a8915cfa1c609b5e7fcd8eb23cdad414a2affd
mihaeladuta <mihaela.duta@psy.ox.ac.uk>
2023-08-03 20:09:44 +0100

    Processed the checkbox entries

--- src/data/util.py
+++ src/data/util.py
@@ -22,6 +22,18 @@ def read_setup_and_data(path='../data/raw/',
     return setup, dset
 
 
+def save_data(dset,
+              path='../data/processed/',
+              fname='rse_group_survey_2023_responses_processed'):
+    """ Save the data frame of the survey responses.
+    Args:
+        dset (pd.DataFrame): data frame of the survey responses
+        path (str): path to the setup and data files
+        fname (str): name of the data file
+    """
+    dset.to_csv(f"{path}{fname}.csv")
+
+
 def validate_setup_and_data(setup, dset):
     """ Validate the setup and data files.
     Args:
@@ -43,3 +55,48 @@ def validate_setup_and_data(setup, dset):
             print(f" ERROR: question {iqst+1} in setup and data files do not match.")
 
     return validated
+
+
+def process_checkboxes(dset, setup):
+    """ Process the checkbox responses.
+    Args:
+        dset (pd.DataFrame): data frame of the survey responses
+        setup (dict): setup dictionary
+    Returns:
+        dset (pd.DataFrame): data frame of the survey responses
+    """
+    for qst in setup.keys():
+        if setup[qst]["type"] != "checkbox":
+            continue
+
+        # fill the empty cells with ""
+        dset[qst] = dset[qst].fillna("")
+
+        # get the options and number of options
+        opts = setup[qst]["options"]
+        nopts = len(opts)
+
+        # check if none of the options are in the response and append "Other"
+        opts_extended = opts + [""] # add empty string to opts to catch empty responses
+        dset[qst] = dset[qst].apply(lambda x: "Other, " + x 
+                                    if all(opt not in x for opt in opts_extended) 
+                                    else x)
+        
+        # create extra columns for each option
+        extra_cols = [f"{qst}_option_{iopt+1}" for iopt in range(nopts)]
+        dset[extra_cols] = ""
+        for iopt, opt in enumerate(opts):
+            dset.loc[dset[qst].str.contains(opt),
+                    f"{qst}_option_{iopt+1}"] = opt 
+        
+        # if we have Other responses, create a new column and populate it
+        # with the given other responses
+        nother = dset[qst].str.contains("Other,").sum()
+        if nother > 0:
+            extra_column = f"{qst}_option_other"
+            dset[extra_column] = ""
+            dset.loc[dset[qst].str.contains("Other"), extra_column] = \
+                dset.loc[dset[qst].str.contains("Other"), qst] 
+            dset.loc[:, extra_column] = dset.loc[:, extra_column].str.replace("Other, ", "")
+    
+    return dset
